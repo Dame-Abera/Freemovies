0e762c6f46ba79fc5ef3b38fc61c99768141e2cb
Dame Abera <dameabera2003@gmail.com>
2025-07-01 15:57:33 +0300

    added  search  functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Search from './components/search.jsx'
-import  {useState} from 'react';
-// import { useState } from 'react'
+import  {useState,useEffect} from 'react';
 import './App.css'
+// import { useState } from 'react' https://api.themoviedb.org/3/trending/movie/{time_window}
+const  API_BASE_URL=`https://api.themoviedb.org/3`;
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+// console.log(API_KEY);
+const API_OPTIONS={
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`}
+}
+
 const App = () => {
   const [searchTerm,setSearchTerm]=useState("to  do  list");
+  const [errorMessage,setErrorMessage]=useState("");
+  useEffect(()=>{
+    
+  },[])
+  const fetchmovies=async()=>{
+    try{
+
+    }catch(error){
+      console.error("Error fetching movies:", error);
+      setErrorMessage(error.message)
+    }
+  }
   return (
     <main>
       <div className="pattern"/>
@@ -15,6 +37,9 @@ const App = () => {
           <h1>Find <span className='text-gradient'>Movies</span> you'll Enjoy without the Hassle</h1>
         </header>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className='text-white text-3xl'>{
+searchTerm ? `Search results for "${searchTerm}"` : "Search for a movie, actor, or director"
+        }</h1>
       </div>
      
       
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Search = ({searchTerm,setSearchTerm}) => {
   return (
-    <div className='text-white text-3xl'>
-      {searchTerm}
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="" />
+        <input 
+          type="text"
+          placeholder='Search for a song, artist, or album'
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}>
+        </input>
+      </div>
     </div>
   )
 }
